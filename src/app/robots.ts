45f63dd04cde9127babf1dac707b2353509c5e1e
HusseinFawaz <HusseinFawaz>
2024-12-22 23:43:23 +0100

    Your message push to repooo END

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots() {
         rules: [
             {
                 userAgent: '*',
-            },
+            },  
         ],
         sitemap: `${baseURL}/sitemap.xml`,
     }
